62b75407cb065d5d82d4dfafdc794c74557d014b
ThomasvLingen <thovali96@gmail.com>
2016-11-15 17:03:08 +0100

    feat: improve test cow

--- src/ExampleGameObject.cpp
+++ src/ExampleGameObject.cpp
@@ -13,6 +13,7 @@ ExampleGameObject::~ExampleGameObject() {
 
 void ExampleGameObject::Update(float deltaTime) {
     (void)deltaTime;
-    const int x = (int)(sin(mApplication->GetTimeSinceStartedMS() / 300.0) * 15.0 + 400);
-    SetOffset(x, 250);
+    const int x = (int)(sin(mApplication->GetTimeSinceStartedMS() / 300.0) * 200.0 + 250);
+    const int y = (int)(cos(mApplication->GetTimeSinceStartedMS() / 300.0) * 200.0 + 250);
+    SetOffset(x, y);
 }
