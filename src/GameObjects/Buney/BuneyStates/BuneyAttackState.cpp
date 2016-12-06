66ee79395234517bfb731af6f37fa359ed4d6ae9
jorgdebont <jacbont@student.avans.nl>
2016-12-06 12:41:40 +0100

    feat: started on attackstate

---
+++ src/GameObjects/Buney/BuneyStates/BuneyAttackState.cpp
@@ -0,0 +1,11 @@
+//
+// Created by jazula on 12/6/16.
+//
+
+#include "BuneyAttackState.hpp"
+
+BuneyAttackState::BuneyAttackState(Buney& context)
+: BuneyState(context)
+{
+    
+}
---
+++ src/GameObjects/Buney/BuneyStates/BuneyAttackState.hpp
@@ -0,0 +1,16 @@
+//
+// Created by jazula on 12/6/16.
+//
+
+#ifndef MINOR_KMINT_BUNEYATTACKSTATE_HPP
+#define MINOR_KMINT_BUNEYATTACKSTATE_HPP
+
+#include "BuneyState.hpp"
+
+class BuneyAttackState : public BuneyState {
+public:
+    BuneyAttackState(Buney& context);
+};
+
+
+#endif //MINOR_KMINT_BUNEYATTACKSTATE_HPP
